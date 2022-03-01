0503014fa79971416667acc338d773b5e18161c0
Shervin <khanim97@gmail.com>
2022-03-01 19:36:26 +0330

    just recognize and reverse

--- README.md
+++ README.md
@@ -3,16 +3,65 @@
 * no phiphon needed 
 * at middle error `promise` in `sketch.js` or `ml5.min.js` occure!
 
-
-
 ## image processing libs
 
 * ml5.js (Now)
 * opencv > opencv.js > cv
 * tensorflow > tensorflow.js > tf
 
+## preview
 
+![](1.png)
 
+## to change
+
+### ctx stroke
+
+``` 
+// ctx.strokeStyle = "blue";
+// ctx.stroke();
+// ctx.lineWidth = 10;
+```
+
+### draw face (in for loop)
+
+```
+// ===================================
+//    stroke face
+// ===================================
+
+const mouth = detections[i].parts.mouth;
+const nose = detections[i].parts.nose;
+const leftEye = detections[i].parts.leftEye;
+const rightEye = detections[i].parts.rightEye;
+const rightEyeBrow = detections[i].parts.rightEyeBrow;
+const leftEyeBrow = detections[i].parts.leftEyeBrow;
+
+//
+// drawPart(mouth, true);
+// drawPart(nose, false);
+// drawPart(leftEye, true);
+// drawPart(leftEyeBrow, false);
+// drawPart(rightEye, true);
+// drawPart(rightEyeBrow, false);
+
+// 
+function drawPart(feature, closed) {
+
+    ctx.beginPath();
+
+    for (let i = 0; i < feature.length; i += 1) {
+    var x = feature[i]._x, y = feature[i]._y;
+    if (i == 0){ctx.moveTo(x, y)} else {ctx.lineTo(x, y)}
+    }
+
+    if (closed == true){ctx.closePath()}
+
+    ctx.stroke();
+
+}
+    
+```
 
 
 
--- src/js/sketch.js
+++ src/js/sketch.js
@@ -6,7 +6,6 @@ window.addEventListener("DOMContentLoaded", async () => {
   // --------------------------------------------------------
   // get stream from webcam
   // --------------------------------------------------------
-
   const stream = await navigator.mediaDevices.getUserMedia({video: true});
   
   const video = document.createElement("video");
@@ -28,7 +27,6 @@ window.addEventListener("DOMContentLoaded", async () => {
   // --------------------------------------------------------
   // Define ML Model 
   // --------------------------------------------------------
-
   const faceapi = await ml5.faceApi(video, {
     withLandmarks: true,
     withDescriptors: false
@@ -43,67 +41,26 @@ window.addEventListener("DOMContentLoaded", async () => {
     if (err) return console.log(err); 
 
     // video to canvas 
-    ctx.fillStyle = "#000000";
+    ctx.fillStyle = `rgb(100, 100, 0)`;
     ctx.fillRect(0, 0, width, height);
-    ctx.drawImage(video, 0, 0, width, height);
-
+ 
     // 
     if (detections && detections.length > 0) {
-      for (let i = 0; i < detections.length; i += 1) {
-
-        // ===================================
-        //    ctx.rect
-        // ===================================
-
+      for (let i = 0; i < detections.length; i++) {
+ 
         const alignedRect = detections[i].alignedRect;
         const x = alignedRect._box._x;
         const y = alignedRect._box._y;
         const boxWidth = alignedRect._box._width;
         const boxHeight = alignedRect._box._height;
-    
-        ctx.beginPath();
-        ctx.rect(x, y, boxWidth, boxHeight);
-        ctx.strokeStyle = "#a15ffb";
-        ctx.stroke();
-        ctx.closePath();
-
-        // ===================================
-        //    stroke face
-        // ===================================
-        
-        const mouth = detections[i].parts.mouth;
-        const nose = detections[i].parts.nose;
-        const leftEye = detections[i].parts.leftEye;
-        const rightEye = detections[i].parts.rightEye;
-        const rightEyeBrow = detections[i].parts.rightEyeBrow;
-        const leftEyeBrow = detections[i].parts.leftEyeBrow;
-    
-        //
-        drawPart(mouth, true);
-        drawPart(nose, false);
-        drawPart(leftEye, true);
-        drawPart(leftEyeBrow, false);
-        drawPart(rightEye, true);
-        drawPart(rightEyeBrow, false);
-    
-        // 
-        function drawPart(feature, closed) {
-    
-          ctx.beginPath();
-    
-          // for (let i = 0; i < feature.length; i += 1) {
-          //   var x = feature[i]._x, y = feature[i]._y;
-          //   if (i == 0){ctx.moveTo(x, y)} else {ctx.lineTo(x, y)}
-          // }
-
-          if (closed == true){ctx.closePath()}
-
-          ctx.stroke();
-    
-        }
-    
-
-
+          
+        // put a piece of video on canvas
+        ctx.drawImage(video, 
+          x-25, y-100, 
+          boxWidth-20, boxHeight+70, 
+          x-25, y-100, 
+          boxWidth-20, boxHeight+70); 
+  
       } 
     }
     
@@ -112,12 +69,7 @@ window.addEventListener("DOMContentLoaded", async () => {
     // ----------------------
     faceapi.detect(drawDetectedFace);
 
-
   }
-
-
-
-
 
 });
   
